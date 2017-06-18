7f2307a4827b1ac882f0e516c3b0a124aa4877d9
Ernesto Villafuerte <ernesto.villafuerte@gmail.com>
2017-06-17 22:53:32 -0300

    Arquivos do curso de Machine Learning on Finance
    Curso de Python aplicado em Finanças utilizando Machine Learning

---
+++ computemaxclosingprice.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def get_max_close(symbol):
+    """Return the maximum closing value for stock indicated by symbol
+    
+    Note: Data for a stock is stored in file:data/<symbol>.csv
+    """
+    df = pd.read_csv("machinelearningfinance/data/{}.csv".format(symbol)) # read in data
+    return df['Close'].max() # compute and return max
+    
+def test_run():
+    """Function called by Test Run."""
+    for symbol in ['AAPL', 'IBM']:
+        print("Max close")
+        print(symbol, get_max_close(symbol))
+        
+if __name__ == "__main__": #if run standalone
+    test_run()
+    
