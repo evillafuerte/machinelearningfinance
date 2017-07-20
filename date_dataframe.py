a2746bf0144484a0807dc51edc1c80db37ea1ee1
Ernesto Villafuerte <ernesto.villafuerte@gmail.com>
2017-07-20 05:31:30 -0300

    20-07-2017 05:31

--- date_dataframe.py
+++ date_dataframe.py
@@ -2,13 +2,18 @@ import pandas as pd
 
 
 def test_run():
-    start_date = '2010-01-22'
-    start_end = '2010-01-26'
+    start_date = '2017-04-22'
+    start_end = '2017-04-26'
     dates = pd.date_range(start_date, start_end)
 
     df1 = pd.DataFrame(index=dates)
-    print df1
 
+    #read SPY in a temporary dataframe
+    dfSPY = pd.read_csv("data/SPY.csv", index_col="Date", parse_dates=True)
+
+    #Join the two dataframes using Dataframe.join()
+    df1 = df1.join(dfSPY)
+    print df1
 
 if __name__ == "__main__":
     test_run()
