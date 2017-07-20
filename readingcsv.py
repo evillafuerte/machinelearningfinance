c7ecfba92d80459e275f5504fd296b339bc4690e
Ernesto Villafuerte <ernesto.villafuerte@gmail.com>
2017-07-20 05:06:15 -0300

    Mac OS python project changes

--- readingcsv.py
+++ readingcsv.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 def test_run():
     """Function called by Test Run."""
-    df = pd.read_csv("machinelearningfinance/data/AAPL.csv")
+    df = pd.read_csv("data/AAPL.csv")
     # TODO: Print last 5 rows of the data frame
-    print(df[5:10])
+    # print(df[5:10])
+    print(df.tail(5))
 
 if __name__ == "__main__":
     test_run()
